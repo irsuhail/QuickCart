4ec3b9e0f991ef1ce028334763c0c43b67c4df25
irsuhail <irsuhail2000@gmail.com>
2025-08-21 14:57:19 +0530

    Fix BoxIcon import and update Clerk keys

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { assets, CartIcon, HomeIcon,BagIcon} from "@/assets/assets";
+import { assets, CartIcon, HomeIcon,BagIcon,BoxIcon} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
